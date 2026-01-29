c88b3cae48ab9d648f0a58e21fb8d08a2e4f821b
saulo <saulolazarogarcia@gmail.com>
2026-01-29 16:28:48 -0300

    cloud run droplet tf

---
+++ terraform/cloud-run/outputs.tf
@@ -0,0 +1,14 @@
+output "service_name" {
+  description = "Cloud Run service name"
+  value       = google_cloud_run_v2_service.web_server.name
+}
+
+output "service_url" {
+  description = "Cloud Run service URL"
+  value       = google_cloud_run_v2_service.web_server.uri
+}
+
+output "latest_ready_revision" {
+  description = "Latest ready revision of the service"
+  value       = google_cloud_run_v2_service.web_server.latest_ready_revision
+}
